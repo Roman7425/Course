5e4a41204e72701c57ad593e858ee4591e336a08
romangubkin1995 <romangubkin1995@gmail.com>
2020-03-07 18:51:12 +0700

    Исправил перевод температур

--- WebCourse/Temperature/Script.js
+++ WebCourse/Temperature/Script.js
@@ -1,31 +1,25 @@
-﻿function ready() {
+﻿document.addEventListener("DOMContentLoaded", function () {
     var errorMessage = document.getElementById("error-message");
-    var resultKelvin = document.createElement("p");
-    var resultFarengate = document.createElement("p");
+    var resultKelvin = document.getElementById("resultKelvin");
+    var resultFahrenheit = document.getElementById("resultFahrenheit");
 
     var button = document.getElementById("button");
     button.addEventListener("click", function (e) {
-
         errorMessage.style.display = "none";
         var temperatureValue = document.getElementById("input-temperature");
         var celsius = temperatureValue.value;
 
         resultKelvin.textContent = "";
-        resultFarengate.textContent = "";
+        resultFahrenheit.textContent = "";
 
         if (celsius === "" || isNaN(celsius)) {
             errorMessage.style.display = "block";
         }
         else {
-            var kelvin = Number(celsius) + 273;
-            var farengate = (Number(celsius) * 9 / 5) + 32;
+            var kelvin = Number(celsius) + 273.15;
+            var fahrenheit = (Number(celsius) * 9 / 5) + 32;
             resultKelvin.textContent = "По шкале Кельвина = " + kelvin;
-            resultFarengate.textContent = "По шкале Фаренгейта = " + farengate;
-            document.body.append(resultKelvin);
-            document.body.append(resultFarengate);
+            resultFahrenheit.textContent = "По шкале Фаренгейта = " + fahrenheit;
         }
-
     });
-}
-
-document.addEventListener("DOMContentLoaded", ready);
+});
