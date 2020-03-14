fffb8654b39d191d56d75215aea2864da0e034b5
romangubkin1995 <romangubkin1995@gmail.com>
2020-03-14 20:22:17 +0700

    Исправил перевод температур

--- WebCourse/Temperature/Script.js
+++ WebCourse/Temperature/Script.js
@@ -1,25 +1,24 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
     var errorMessage = document.getElementById("error-message");
-    var resultKelvin = document.getElementById("resultKelvin");
-    var resultFahrenheit = document.getElementById("resultFahrenheit");
+    var resultKelvin = document.getElementById("result-kelvin");
+    var resultFahrenheit = document.getElementById("result-fahrenheit");
 
     var button = document.getElementById("button");
-    button.addEventListener("click", function (e) {
+    button.addEventListener("click", function () {
         errorMessage.style.display = "none";
-        var temperatureValue = document.getElementById("input-temperature");
-        var celsius = temperatureValue.value;
+        var temperatureInput = document.getElementById("input-temperature");
+        var celsiusTemperature = temperatureInput.value;
 
         resultKelvin.textContent = "";
         resultFahrenheit.textContent = "";
 
-        if (celsius === "" || isNaN(celsius)) {
+        if (celsiusTemperature === "" || isNaN(celsiusTemperature)) {
             errorMessage.style.display = "block";
-        }
-        else {
-            var kelvin = Number(celsius) + 273.15;
-            var fahrenheit = (Number(celsius) * 9 / 5) + 32;
-            resultKelvin.textContent = "По шкале Кельвина = " + kelvin;
-            resultFahrenheit.textContent = "По шкале Фаренгейта = " + fahrenheit;
+        } else {
+            var kelvinTemperature = Number(celsiusTemperature) + 273.15;
+            var fahrenheitTemperature = (Number(celsiusTemperature) * 9 / 5) + 32;
+            resultKelvin.textContent = "По шкале Кельвина = " + kelvinTemperature;
+            resultFahrenheit.textContent = "По шкале Фаренгейта = " + fahrenheitTemperature;
         }
     });
 });
