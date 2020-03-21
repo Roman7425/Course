9f47366260e2ab864d19e4e523a54e7f6cf34d8a
romangubkin1995 <romangubkin1995@gmail.com>
2020-03-21 15:45:20 +0700

    Исправил TODOList

---
+++ WebCourse/PhoneBook/Script.js
@@ -0,0 +1,12 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    var nameInput = document.getElementById("input-name");
+    var surnameInput = document.getElementById("input-surname");
+    var numberInput = document.getElementById("input-number");
+    var tableBody = document.getElementById("table-body");
+    var addButton = document.getElementById(add - button);
+
+    addButton.addEventListener("click", function () {
+        var newTr = document.createElement("tr");
+
+    });
+});
--- WebCourse/TODOList/Script.js
+++ WebCourse/TODOList/Script.js
@@ -1,58 +1,62 @@
-﻿function ready() {
+﻿document.addEventListener("DOMContentLoaded", function () {
     var addButton = document.getElementById("add-new-task-button");
     var inputNewTask = document.getElementById("new-task-input");
     var list = document.querySelector(".task-list>ul");
-    var errorMessage = document.getElementById("error-message");
+    var errorMessage = document.getElementById("error-message-emptiness");
 
-    addButton.addEventListener("click", function (e) {
-
+    addButton.addEventListener("click", function () {
         errorMessage.style.display = "none";
 
         if (inputNewTask.value === "") {
-            errorMessage.style.display = "block";
+            return errorMessage.style.display = "block";
         }
-        else {
-            var inputNewTaskValue = inputNewTask.value;
-            var newTask = document.createElement("li");
-            var deleteButton = document.createElement("button");
-            var createButton = document.createElement("button");
-            deleteButton.textContent = "Удалить";
-            createButton.textContent = "Редактировать";
+        var inputNewTaskValue = inputNewTask.value;
+        var newTask = document.createElement("li");
+        var deleteButton = document.createElement("button");
+        var createButton = document.createElement("button");
+        deleteButton.textContent = "Удалить";
+        createButton.textContent = "Редактировать";
 
-            newTask.textContent = inputNewTaskValue;
-            list.append(newTask);
-            inputNewTask.value = "";
-            newTask.append(deleteButton);
-            newTask.append(createButton);
+        newTask.textContent = inputNewTaskValue;
+        list.append(newTask);
+        inputNewTask.value = "";
+        newTask.append(deleteButton);
+        newTask.append(createButton);
 
-            deleteButton.addEventListener("click", function (e) {
-                list.removeChild(newTask);
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(newTask);
+        });
+
+        createButton.addEventListener("click", function () {
+            var acceptCreateButton = document.createElement("button");
+            var cancelButton = document.createElement("button");
+
+            acceptCreateButton.textContent = "OK";
+            cancelButton.textContent = "Отмена";
+
+            newTask.innerHTML = "<input/>";
+            newTask.append(acceptCreateButton);
+            newTask.append(cancelButton);
+
+            var createInput = list.querySelector("input");
+            createInput.value = inputNewTaskValue;
+
+            acceptCreateButton.addEventListener("click", function () {
+                if (createInput.value === "") {
+                    return list.removeChild(newTask);
+                }
+                var createValue = createInput.value;
+                inputNewTaskValue = createInput.value;
+                newTask.textContent = createValue;
+                newTask.append(deleteButton);
+                newTask.append(createButton);
             });
 
-            createButton.addEventListener("click", function (e) {
-                var acceptCreateButton = document.createElement("button");
-                acceptCreateButton.textContent = "OK";
-
-                newTask.innerHTML = "<input class=\"create-input\" />";
-                newTask.append(acceptCreateButton);
-
-                var createInput = list.querySelector("input");
-                createInput.value = inputNewTaskValue;
-
-                acceptCreateButton.addEventListener("click", function (e) {
-                    if (createInput.value === "")
-                    {
-                        list.removeChild(newTask);
-                    }
-                    var createValue = createInput.value;
-                    inputNewTaskValue = createInput.value;
-                    newTask.textContent = createValue;
-                    newTask.append(deleteButton);
-                    newTask.append(createButton);
-                });
+            cancelButton.addEventListener("click", function () {
+                newTask.textContent = inputNewTaskValue;
+                newTask.append(deleteButton);
+                newTask.append(createButton);
             });
-        }
+        });
     });
-}
-
-document.addEventListener("DOMContentLoaded", ready);
+});
