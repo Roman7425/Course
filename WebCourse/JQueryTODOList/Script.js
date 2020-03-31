e0d716cc73b6d4dd7f967c631a8604ed32b25704
romangubkin1995 <romangubkin1995@gmail.com>
2020-03-31 12:10:18 +0700

    Исправил JQueryToDoList

--- WebCourse/JQueryTODOList/Script.js
+++ WebCourse/JQueryTODOList/Script.js
@@ -2,59 +2,67 @@
     var inputNewTask = $("#new-task-input");
     var list = $(".task-list > ul");
     var errorMessage = $("#error-message-emptiness");
-    var addButton = $("#add-new-task-button")
-        .on("click", function () {
-            errorMessage.css("display", "none");
-            if (inputNewTask.val() === "") {
-                errorMessage.css("display", "block");
-                return;
-            }
-            var newTask = $("<li></li>")
-                .appendTo(list);
-            var newTaskText = $("<p></p>")
-                .appendTo(newTask);
-            var newTaskValue = inputNewTask.val();
-            newTaskText.text(newTaskValue);
-            var deleteButton = $("<button>Удалить</button>")
-                .on("click", function () {
-                    newTask.remove();
-                })
-                .appendTo(newTask);
-            var createButton = $("<button>Редактировать</button>")
-                .appendTo(newTask)
-                .click(function () {
-                    var createInput = $("<input/>")
-                        .appendTo(newTask)
-                        .val(newTaskValue);
-                    deleteButton.hide();
-                    createButton.hide();
-                    newTaskText.text("");
-                    var acceptButton = $("<button>OK</button>")
-                        .appendTo(newTask)
-                        .click(function () {
-                            if (createInput.val() === "") {
-                                newTask.remove();
-                                return;
-                            }
-                            newTaskText.text(createInput.val());
-                            newTaskValue = createInput.val();
-                            deleteButton.show();
-                            createButton.show();
-                            createInput.hide();
-                            acceptButton.hide();
-                            cancelButton.hide();
-                        });
-                    var cancelButton = $("<button>Отмена</button>")
-                        .appendTo(newTask)
-                        .click(function () {
-                            newTaskText.text(newTaskValue);
-                            deleteButton.show();
-                            createButton.show();
-                            createInput.hide();
-                            acceptButton.hide();
-                            cancelButton.hide();
-                        });
-                });
-            inputNewTask.val("");
-        });
+
+    $("#add-new-task-button").on("click", function () {
+        errorMessage.hide();
+        if (inputNewTask.val() === "") {
+            errorMessage.show();
+            return;
+        }
+        var newTask = $("<li></li>")
+            .appendTo(list);
+
+        var newTaskText = $("<p></p>")
+            .appendTo(newTask);
+
+        var newTaskValue = inputNewTask.val();
+        newTaskText.text(newTaskValue);
+
+        var deleteButton = $("<button>Удалить</button>")
+            .on("click", function () {
+                newTask.remove();
+            })
+            .appendTo(newTask);
+
+        var editButton = $("<button>Редактировать</button>")
+            .appendTo(newTask)
+            .click(function () {
+                var createInput = $("<input/>")
+                    .appendTo(newTask)
+                    .val(newTaskValue);
+
+                deleteButton.hide();
+                editButton.hide();
+                newTaskText.text("");
+
+                var acceptButton = $("<button>OK</button>")
+                    .appendTo(newTask)
+                    .click(function () {
+                        if (createInput.val() === "") {
+                            newTask.remove();
+                            return;
+                        }
+
+                        newTaskText.text(createInput.val());
+                        newTaskValue = createInput.val();
+                        deleteButton.show();
+                        editButton.show();
+                        createInput.remove();
+                        acceptButton.remove();
+                        cancelButton.remove();
+                    });
+
+                var cancelButton = $("<button>Отмена</button>")
+                    .appendTo(newTask)
+                    .click(function () {
+                        newTaskText.text(newTaskValue);
+                        deleteButton.show();
+                        editButton.show();
+                        newTask.append(createInput)
+                            .append(acceptButton)
+                            .append(cancelButton);
+                    });
+            });
+        inputNewTask.val("");
+    });
 });
--- WebCourse/PhoneBook/Script.js
+++ WebCourse/PhoneBook/Script.js
@@ -1,12 +1,91 @@
 ﻿document.addEventListener("DOMContentLoaded", function () {
-    var nameInput = document.getElementById("input-name");
-    var surnameInput = document.getElementById("input-surname");
-    var numberInput = document.getElementById("input-number");
-    var tableBody = document.getElementById("table-body");
-    var addButton = document.getElementById(add - button);
+    var tableBody = (".table-body");
+    var addButton = $("#add-button")
+        .click(function () {
+            $("#name-error").css("display", "none");
+            $("#surname-error").css("display", "none");
+            $("#number-error").css("display", "none");
+            $("#number-repeated-error").css("display", "none");
+            var wasError = false;
+            var nameInput = $("#input-name").css("border-color", "");
+            var surnameInput = $("#input-surname").css("border-color", "");
+            var numberInput = $("#input-number").css("border-color", "");
 
-    addButton.addEventListener("click", function () {
-        var newTr = document.createElement("tr");
+            if (nameInput.val() === "") {
+                $("#name-error").css("display", "block");
+                nameInput.css("border-color", "#eb5138");
+                wasError = true;
+            }
+            if (surnameInput.val() === "") {
+                $("#surname-error").css("display", "block");
+                surnameInput.css("border-color", "#eb5138");
+                wasError = true;
+            }
+            if (numberInput.val() === "" || isNaN(numberInput.val())) {
+                $("#number-error").css("display", "block");
+                numberInput.css("border-color", "#eb5138");
+                wasError = true;
+            }
 
+            $(".table-body .number-col").each(function () {
+                if ($(this).text() === numberInput.val()) {
+                    $("#number-repeated-error").css("display", "block");
+                    numberInput.css("border-color", "#eb5138");
+                    wasError = true;
+                }
+            });
+
+            if (wasError === true) {
+                return;
+            }
+
+            var newTr = $("<tr></tr>")
+                .appendTo(tableBody)
+                .append($("<td><input class=\"check\" type=\"checkbox\"></td>"))
+                .append($("<td></td>"))
+                .append($("<td></td>").text(nameInput.val()))
+                .append($("<td></td>").text(surnameInput.val()))
+                .append($("<td class=\"number-col\"></td>").text(numberInput.val()))
+                .append($("<td></td>").html($("<button>Удалить</button>")
+                    .click(function () {
+                        newTr.remove();
+                        $(".table-body tr").each(function (i) {
+                            $(this).children().eq(1).text(i + 1);
+                        });
+                    })));
+
+            $(".table-body tr").each(function (i) {
+                $(this).children().eq(1).text(i + 1);
+            });
+
+            nameInput.val("");
+            surnameInput.val("");
+            numberInput.val("");
+            wasError = true;
+        });
+
+    var deleteButtom = $("#head-delete-button")
+        .click(function () {
+            $(".check").each(function () {
+                if ($(this).is(":checked")) {
+                    $(this).parent().parent().remove();
+                }
+            });
+            $(".table-body tr").each(function (i) {
+                $(this).children().eq(1).text(i + 1);
+            });
+        });
+
+    $("#head-check").click(function () {
+        if ($("#head-check").is(":checked") === true) {
+            $(".check").each(function () {
+                $(this).prop("checked", true);
+            });
+        }
+        else {
+            $(".check").each(function () {
+                $(this).prop("checked", false);
+            });
+        }
     });
 });
